154618ff61099415de2b4e36ea60735ab9d1d245
Zuzi <zuzuzka@gmail.com>
2013-10-26 19:29:50 +0200

    log-in fix, style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_name(params[:name])
 	if user and user.authenticate(params[:password])
 		session[:user_id] = user.id
-		redirect_to root_path
+		redirect_to root_path , notice: "Logged in"
 	else
 		redirect_to login_url, alert: "Invalid user/password combination"
   end
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
   	session[:user_id] = nil
-	redirect_to store_url, notice: "Logged out"
+	redirect_to root_path, notice: "Logged out"
   end
 end
