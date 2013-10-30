3bbed1fc8d87b36933b22402bdfb6117f9e4041c
Zuzi <zuzuzka@gmail.com>
2013-10-30 15:15:15 +0100

    add annotation, connect category to action, validation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,26 @@
+# == Schema Information
+#
+# Table name: activities
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  abstract    :text
+#  detail      :text
+#  category_id :integer
+#  note        :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Activity < ActiveRecord::Base
+  validates :title, presence: true
+  validate :category_exists
+
+  belongs_to :category
+
+  private
+  def category_exists
+    errors.add(:base, "Category must exist") unless Category.find_by_id(self.category_id)
+  end
+
 end
--- test/unit/activity_test.rb
+++ test/unit/activity_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: activities
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  abstract    :text
+#  detail      :text
+#  category_id :integer
+#  note        :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ActivityTest < ActiveSupport::TestCase
